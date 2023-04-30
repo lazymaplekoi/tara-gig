8e2cb7dd93ba1eb3639250974657a04d4f123de5
Mikayla Lopez <lopez@greenova.de>
2023-04-30 22:13:54 +0800

    set up static assets + barebones index page

--- src/gigs/urls.py
+++ src/gigs/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('error/', views.error, name='error'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
