88390ecb507095b5f53af88e1569cc2f387cbdd5
Mikayla Lopez <lopez@greenova.de>
2023-08-29 21:51:56 +0800

    add display name to Venue

--- src/gigs/models.py
+++ src/gigs/models.py
@@ -24,7 +24,8 @@ class City(models.Model):
         return self.name
 
 class Venue(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=50)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
 
